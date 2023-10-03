6115997f4dba03dcd3e652030f8105c5af0d319b
leong <leong@DESKTOP-A61DB78>
2023-10-02 21:15:52 -0400

    Agregar archivos de proyecto.

---
+++ Practica 03 - Ejemplos de Arreglos c++/Practica 03 - Ejemplos de Arreglos c++.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main() {
+
+    int numeros[6] = { 10,20,30,40,50,60 };
+    int n[] = { 3,4,5 };
+    char c[] = { 'L', 'u', 'i' ,'s' };
+
+    char s[] = "Mona Lisa";
+
+
+
+
+
+
+
+
+
+    cout << "El arreglo de tipo entero de 6 elementos es:" << "--->" << numeros[0] << "," << numeros[1] << "," << numeros[2] << "," << numeros[3]
+        << numeros[4] << "," << numeros[5] << "," << numeros[6] << endl;
+
+    cout << "El arreglo de tipo entero de 3 elementos es:" << "--->" << n[0] << "," << n[1] << "," << n[2] << endl;
+
+
+    cout << "El arreglo de tipo char de 4 elementos es:" << "---->" << c[1] << "," << c[2] << "," << c[3] << endl;
+
+
+    cout << "El arreglo de tipo char Molalisa es :" << "--------->" << s[0] << "," << s[1] << "," << s[2] << "," << s[3] << "," << s[4] << "," << s[5] << "," << s[6] << endl;
+
+
+
+
+
+
+
+
+    return 0;
+}
